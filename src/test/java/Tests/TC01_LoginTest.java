3b9bb4de8f3fabde6651b5b925fd7f88ebdf0864
amfmh <ahmedmotaz999@gmail.com>
2024-12-30 20:14:24 +0200

    adding always run

--- src/test/java/Tests/TC01_LoginTest.java
+++ src/test/java/Tests/TC01_LoginTest.java
@@ -23,7 +23,7 @@ public class TC01_LoginTest {
     private final String USERNAME = DataUtils.getJsonData("validLogin", "username");
     private final String PASSWORD = DataUtils.getJsonData("validLogin", "password");
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setup() throws IOException {
         //condition ? true : false
         String browser = System.getProperty("browser") != null ? System.getProperty("browser") : getPropertyValue("environment", "Browser");
@@ -44,7 +44,7 @@ public class TC01_LoginTest {
         Assert.assertTrue(new P01_LoginPage(getDriver()).assertLoginTc(getPropertyValue("environment", "HOME_URL")));
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void quit() {
         quitDriver();
     }
